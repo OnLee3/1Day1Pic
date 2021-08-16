8b51a5e8624692a6d6d8a6036d700159ee2ad0bc
OnLee3 <dlwotjr2@gmail.com>
2021-08-16 22:55:31 +0900

    join, login 페이지 생성

---
+++ src/controllers/userController.js
@@ -0,0 +1,8 @@
+
+export const join = (req, res) => {
+    res.render("join")
+}
+
+export const login = (req, res) => {
+    res.render("login")
+}
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { home } from "../controllers/picController.js"
+import { join, login } from "../controllers/userController.js";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
+rootRouter.get("/join", join)
+rootRouter.get("/login", login);
 
 export default rootRouter;
